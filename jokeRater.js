2faf31fe6586dda0710c7b786d5be4ef1c9c3dd0
Älva <alvlac0326@skola.goteborg.se>
2024-03-28 09:20:27 +0100

    got joke to print on screen

--- jokeRater.js
+++ jokeRater.js
@@ -1,4 +1,27 @@
-fetch('https://icanhazdadjoke.com/',
-    { headers: { 'Accept': 'application/json', 'User-Agent': 'yrgo-kprog-class' } })
-    .then(resp => resp.json())
-    .then(json => console.log(json.joke))
+// fetch('https://icanhazdadjoke.com/',
+// {headers: {'Accept': 'application/json', 'User-Agent': 'yrgo-kprog-class'}})
+// .then(resp => resp.json())
+// .then(json => console.log(json.joke))
+
+
+
+// async function updateFortune() {
+//     let resp = await fetch('/api/fortune')
+//     let json = await resp.json()
+//     let fortune = document.getElementById('fortune')
+//     fortune.innerText = json.fortune
+//     fortune.style.visibility = 'visible';
+// }
+
+
+async function printjoke(){
+    let resp = await fetch('https://icanhazdadjoke.com/', {
+        headers: {'Accept': 'application/json', 'User-Agent': 'yrgo-kprog-class'}
+    })
+    let json = await resp.json()
+    let joke = document.getElementById('joke')
+    joke.innerText = json.joke
+    joke.style.visibility = 'visible';
+}
+
+console.log(printjoke())
